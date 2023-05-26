ea7fcb7281516c66a5bddc7c315768b94512d6c8
mayapandit <pndt.maya1506@gmail.com>
2023-05-26 21:27:04 +0530

    New work content

--- src/test/java/mapping/Demo.java
+++ src/test/java/mapping/Demo.java
@@ -2,8 +2,10 @@ package mapping;
 
 public class Demo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
+		System.out.println("workndone");
 
 	}
 
---
+++ src/test/java/mapping/Personal_banking.java
@@ -0,0 +1,11 @@
+package mapping;
+
+public class Personal_banking
+{
+public static void main(String[] args)
+{	
+	System.out.println("Personal banking functionality");
+	
+}
+
+}
