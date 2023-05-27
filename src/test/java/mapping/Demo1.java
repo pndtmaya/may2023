276798ac277ffb6ef58b848759433ce0dad8a5ac
mayapandit <pndt.maya1506@gmail.com>
2023-05-27 09:38:34 +0530

    By mr Prashant

--- src/test/java/mapping/Demo1.java
+++ src/test/java/mapping/Demo1.java
@@ -1,11 +1,12 @@
 package mapping;
 
-public class Demo {
+public class Demo1 
+{
 
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		System.out.println("workndone");
+		System.out.println("workndone by Mr prashant");
 
 	}
 
